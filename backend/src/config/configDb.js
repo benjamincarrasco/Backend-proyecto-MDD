dc092e25ed8508de56b64017959357d685573106
GabontonioUBB <gabriel.vigueras2201@alumnos.ubiobio.cl>
2025-07-29 03:17:28 -0400

    Agregar CRUD Reuniones

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -5,7 +5,7 @@ import { DATABASE, DB_USERNAME, HOST, PASSWORD } from "./configEnv.js";
 // Configuración de la conexión a la base de datos
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: `pgsqltrans.face.ubiobio.cl`,
+    host: `${HOST}`,
     port: 5432,
     username: `${DB_USERNAME}`,
     password: `${PASSWORD}`,
